5c3e26a7b2b3e4078ecbd65dea2d4ed00787eef3
Yanis944 <yanisrouabah1602@gmail.com>
2021-12-02 11:45:34 +0100

    Part 2

---
+++ src/main/java/fr/lernejo/guessgame/Launcher.java
@@ -0,0 +1,18 @@
+package fr.lernejo.guessgame;
+
+import java.security.SecureRandom;
+
+public class Launcher {
+
+    public static void main(String[] args){
+        Simulation simulation = new Simulation(new HumanPlayer());
+        SecureRandom random  = new SecureRandom();
+        long randomNumber = random.nextInt(100);
+        simulation.initialize(randomNumber);
+        System.out.println("Entrez un chiffre");
+        simulation.loopUntilPlayerSucceed();
+    }
+}
+
+
+
