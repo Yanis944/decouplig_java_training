c6d8a70192eb28f7faa34c8081a5f1afa95d21fa
Yanis944 <yanisrouabah1602@gmail.com>
2021-12-06 11:38:52 +0100

    Part 6

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -5,7 +5,7 @@ import java.util.function.Predicate;
 public class LoggerFactory {
 
     public static Logger getLogger(String name) {
-        return new ContextualLogger(new FileLogger("fileLog"), name);
+        return new CompositeLogger(new ContextualLogger(new FileLogger("fileLog"), name), new ContextualLogger(new ConsoleLogger(), name));
     }
 
 }
