8b010a337a00ff7cff2bef8d6857e91bbe272cdd
Yanis944 <yanisrouabah1602@gmail.com>
2021-12-06 11:41:23 +0100

    Part 7

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -5,7 +5,8 @@ import java.util.function.Predicate;
 public class LoggerFactory {
 
     public static Logger getLogger(String name) {
-        return new CompositeLogger(new ContextualLogger(new FileLogger("fileLog"), name), new ContextualLogger(new ConsoleLogger(), name));
+        Predicate<String> condition = message -> message.contains("simulation");
+        return new CompositeLogger(new ContextualLogger(name, new FilteredLogger(new FileLogger("fileLog"), condition)), new ContextualLogger(name, new ConsoleLogger()));
     }
 
 }
